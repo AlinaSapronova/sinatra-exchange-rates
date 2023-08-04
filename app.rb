1539bbe07f6626839ef62fda9cf311835fbbeab3
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-08-04 18:05:02 +0000

    home page done

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'net/http'
+require 'json'
+
+
+
+convert_url  = "https://api.exchangerate.host/convert?from=USD&to=EUR"
+convert_uri = URI(convert_url)
+convert_response = Net::HTTP.get(convert_uri)
+conv_response_obj = JSON.parse(convert_response)
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+url = "https://api.exchangerate.host/symbols"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+response_obj = JSON.parse(response)
+@symbols = response_obj.fetch("symbols")
+erb(:home)
+end
+
+get("/:symbol") do
+  url = "https://api.exchangerate.host/symbols"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+response_obj = JSON.parse(response)
+  @symbol_name = response_obj.fetch("symbols")
 end
