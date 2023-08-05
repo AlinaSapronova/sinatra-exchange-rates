c191429ed8a2a927e16bd37b4974428a491ec80e
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-08-05 03:15:38 +0000

    finish with currency rate

--- app.rb
+++ app.rb
@@ -3,56 +3,42 @@ require "sinatra/reloader"
 require 'net/http'
 require 'json'
 
+def fetch_symbols
+  url = "https://api.exchangerate.host/symbols"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  response_obj = JSON.parse(response)
+  symbols_hash = response_obj.fetch("symbols")
+  list_of_symbols = symbols_hash.keys
+
+  return list_of_symbols
+end
 
 
 get("/") do
-url = "https://api.exchangerate.host/symbols"
-uri = URI(url)
-response = Net::HTTP.get(uri)
-response_obj = JSON.parse(response)
-@symbols = response_obj.fetch("symbols")
-erb(:home)
+
+  @symbols = fetch_symbols
+
+  erb(:home)
 end
 
-get("/USD") do
-url = "https://api.exchangerate.host/symbols"
-uri = URI(url)
-response = Net::HTTP.get(uri)
-response_obj = JSON.parse(response)
-@symbols = response_obj.fetch("symbols")
-erb(:convert_usd)
+get("/:from") do
+
+  @symbols = fetch_symbols
+
+  erb(:convert)
 end
-
-
-# get("/INR") do
-# url = "https://api.exchangerate.host/symbols"
-# uri = URI(url)
-# response = Net::HTTP.get(uri)
-# response_obj = JSON.parse(response)
-# @symbols = response_obj.fetch("symbols")
-# erb(:convert_inr)
-# end
-
-# get("/KES") do
-# url = "https://api.exchangerate.host/symbols"
-# uri = URI(url)
-# response = Net::HTTP.get(uri)
-# response_obj = JSON.parse(response)
-# @symbols = response_obj.fetch("symbols")
-# erb(:convert_kes)
-# end
-
 
 
 get("/:from/:to") do
-  convert_url  = "https://api.exchangerate.host/convert?from=USD&to=EUR"
-  convert_uri = URI(convert_url)
-  convert_response = Net::HTTP.get(convert_uri)
-  conv_response_obj = JSON.parse(convert_response)
-  currency = conv_response_obj.fetch("query")
-  @from = currency.fetch("from")
-  @to = currency.fetch("to")
-  @result = conv_response_obj.fetch("result")
 
-  erb(:convert)
+  @symbols = fetch_symbols
+
+  url = "https://api.exchangerate.host/convert?from=#{params.fetch("from")}&to=#{params.fetch("to")}"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  response_obj = JSON.parse(response)
+  @rate = response_obj.fetch("info").fetch("rate")
+
+ erb(:convert_currency)
 end
